c8aabc7ec136dfafa4effefed58e181ce748487c
Equation <dihar73@gmail.com>
2024-06-27 18:21:23 +0300

    feat: add gulp-sass in gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,25 @@
-const gulp = require('gulp');
+const {src, dest, watch} = require('gulp');
 const browserSync = require('browser-sync').create();
+const sass = require('gulp-sass')(require('sass'));
 
-gulp.task('hello', function(done) {
-  console.log('Привет мир!');
-  done();
-});
 
-gulp.task('browser-sync', function() {
+function bs() {
+  serveSass();
   browserSync.init({
-      server: {
-          baseDir: "./"
-      }
+    server: {
+      baseDir: "./"
+    }
   });
-  gulp.watch("./*.html").on('change', browserSync.reload);
-});
+  watch("./*.html").on('change', browserSync.reload);
+  watch("./sass/**/*.sass", serveSass);
+  watch("./js/*.js").on('change', browserSync.reload);
+};
+
+function serveSass() {
+  return src("./sass/*.sass")
+    .pipe(sass())
+    .pipe(dest("./css"))
+    .pipe(browserSync.stream());
+};
+
+exports.serve = bs;
