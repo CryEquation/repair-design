051fa40cfd120e6eca95f7b9f40e575f9ebb0c82
Equation <dihar73@gmail.com>
2024-07-13 18:04:57 +0300

    feat: add completed projects block

--- js/main.js
+++ js/main.js
@@ -25,4 +25,29 @@ $(document).ready(function() {
         modal.toggleClass('modal--visible')
     });
 
+    const swiper = new Swiper('.swiper', {
+        // Optional parameters
+        loop: true,
+
+
+        // If we need pagination
+        pagination: {
+            el: '.swiper-pagination',
+            type: 'bullets',
+        },
+
+        // Navigation arrows
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    })
+
+    var next = $('.swiper-button-next');
+    var prev = $('.swiper-button-prev');
+    var bullets = $('.swiper-pagination');
+
+
+
+
 });
