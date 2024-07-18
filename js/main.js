00a9ee61cd4e22476f977ab5a316e87264c312b0
Equation <dihar73@gmail.com>
2024-07-18 15:33:53 +0300

    feat: add section footer

--- js/main.js
+++ js/main.js
@@ -49,5 +49,75 @@ $(document).ready(function() {
 
     new WOW().init();
 
-
+    $('.modal__form').validate({
+        errorClass: "invalid",
+        rules: {
+            userName: {
+                required: true,
+                minlength: 2
+            },
+            userPhone: "required",
+            userEmail: {
+                required: true,
+                email: true
+            }
+        },
+        messages: {
+            userName: {
+                required: "Имя обязательно",
+                minlength: "Нужно больше 2 символов"
+            },
+            userPhone: "Телефон обязательно",
+            userEmail: {
+                required: "Обязательно укажите email",
+                email: "Введите в формате name@domain.com"
+            }
+        }
+    });
+    $('.modal__control').validate({
+        errorClass: "invalid",
+        rules: {
+            userName: {
+                required: true,
+                minlength: 2
+            },
+            userPhone: "required",
+        },
+        messages: {
+            userName: {
+                required: "Имя обязательно",
+                minlength: "Нужно больше 2 символов"
+            },
+            userPhone: "Телефон обязательно",
+        }
+    });
+    $('.modal__footer').validate({
+        errorClass: "invalid",
+        rules: {
+            userName: {
+                required: true,
+                minlength: 2
+            },
+            userQuestion: {
+                required: true,
+                minlength: 5
+            },
+            userPhone: "required",
+            userEmail: {
+                required: true,
+            }
+        },
+        messages: {
+            userName: {
+                required: "Имя обязательно",
+                minlength: "Нужно больше 2 символов"
+            },
+            userPhone: "Телефон обязательно",
+            userQuestion: {
+                required: "Вопрос обязателен",
+                minlength: "Нужно больше 5 символов"
+            },
+        }
+    });
+    $('[type=tel]').mask('+7(000)000-00-00', { placeholder: "+7(___) ___-__-__" });
 });
